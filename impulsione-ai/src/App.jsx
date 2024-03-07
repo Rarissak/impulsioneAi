fe01d8ed9ccc6df3af1df3e7cab7eb6c834e571e
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-07 16:23:26 -0300

    Estruturação do projeto

---
+++ impulsione-ai/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <p>só pra ver</p>
+      </div>
+    </>
+  )
+}
+
+export default App
