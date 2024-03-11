17d842b24ac344e0daa3e0c02b30e502388c1342
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-11 16:46:56 -0300

    Revisão dos componentes barraLinks e button

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Carrossel from './components/carrossel/carrossel.jsx'
 import Header from './components/header/header.jsx'
+import BarraLinks from './components/barraLinks/BarraLinks.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header></Header>
+    <BarraLinks></BarraLinks>
     <Carrossel></Carrossel>
+    <div>
+      <h1>TESTE</h1>
+    </div>
   </React.StrictMode>,
 )
