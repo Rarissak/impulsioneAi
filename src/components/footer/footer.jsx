7f609adae8c9e6da5c53e97fcef3d310c5e81e96
Rarissak <124792677+Rarissak@users.noreply.github.com>
2024-03-12 08:12:55 -0300

    edit feat: Revisão componente footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import './footer.css';
-import logo from '../../assets/logoFooter.svg'
+import LogoBranca from '../../assets/Logo-branca.svg'
 import youtube from '../../icons/iconYoutube.svg'
 import twitter from '../../icons/iconTwitter.svg'
 import linkedin from '../../icons/iconLinkedin.svg'
 import facebook from '../../icons/iconFacebook.svg'
 import instagram from '../../icons/iconInstagram.svg'
-import telegram from '../../icons/iconTelegram.svg'
+import telegram from '../../icons/iconTelegram.svg' 
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <img className="logo" src={logo} alt="Logo" />
-
-        <p id="copyright">Todos os direitos reservados &copy; 2024</p>
+        <img className="logo" src={LogoBranca} alt="Logo" />
 
         <div className="redes-links">
-            <p className="redes-links_p-redes">Acompanhe Nossas Redes</p>
-            <div className="redes-links_img-redes">
-                <a href=""><img src={youtube} alt="Youtube"/></a>
-                <a href=""><img src={twitter} alt="Twitter"/></a>
-                <a href=""><img src={linkedin} alt="LinkedIn"/></a>
-                <a href=""><img src={facebook} alt="Facebook"/></a>
-                <a href=""><img src={instagram} alt="Instagram"/></a>
-                <a href=""><img src={telegram} alt="Telegram"/></a>
-            </div>
+          <p className="redes-links_p-redes">Acompanhe Nossas Redes</p>
+          <div className="redes-links_img-redes">
+            <a href=""><img src={youtube} alt="Youtube"/></a>
+            <a href=""><img src={twitter} alt="Twitter"/></a>
+            <a href=""><img src={linkedin} alt="LinkedIn"/></a>
+            <a href=""><img src={facebook} alt="Facebook"/></a>
+            <a href=""><img src={instagram} alt="Instagram"/></a>
+            <a href=""><img src={telegram} alt="Telegram"/></a>
+          </div>
         </div>
       </div>
+      
+      <p id="copyright">Todos os direitos reservados &copy; 2024</p>
+      
     </footer>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -12,14 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Header></Header>
     <BarraLinks></BarraLinks>
     <Carrossel></Carrossel>
-    <div>
-      <h1>TESTE</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus laudantium maxime aliquam. Sunt repellendus, reprehenderit error architecto iste nemo sed, deleniti voluptas quaerat dolorum quia ducimus asperiores impedit vitae beatae!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius eveniet fugiat tenetur mollitia cumque sunt quisquam cupiditate unde libero non, rerum numquam blanditiis explicabo. Molestiae exercitationem maiores velit assumenda alias?<br/>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde inventore totam rerum nostrum enim tempora aliquam minima necessitatibus natus accusantium quia eos recusandae quam distinctio impedit magni, sed fugiat aperiam.<br/>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis esse delectus aspernatur magnam recusandae harum at perspiciatis eaque labore nulla impedit est, voluptate officiis fuga dolorem doloremque molestiae nihil praesentium.<br/>
-      </p>
-    </div>
     <Footer></Footer>
   </React.StrictMode>,
 )
