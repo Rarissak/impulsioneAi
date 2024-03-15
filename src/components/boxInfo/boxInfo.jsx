6390a21f3230dbb0e3f1335268a7e2cfb8acc760
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-14 22:26:03 -0300

    edit feat: Alteração do componente boxInfo

--- src/components/boxInfo/boxInfo.jsx
+++ src/components/boxInfo/boxInfo.jsx
@@ -1,31 +1,20 @@
 import './boxInfo.css';
 import '../../index.css';
-import GrowIcon from '../../assets/growIcon.svg';
-import StoreIcon from '../../assets/storeIcon.svg';
-import PuzzleIcon from '../../assets/puzzleIcon.svg'; 
-import Informations from '../informations/informations.jsx';
+// import GrowIcon from '../../assets/growIcon.svg';
+// import StoreIcon from '../../assets/storeIcon.svg';
+// import PuzzleIcon from '../../assets/puzzleIcon.svg'; 
+// import Informations from '../informations/informations.jsx';
 
-function BoxInfo() {
+function BoxInfo({title}) {
     return(
         <section>
             <div id="titleBox">
-                <h1 className="title">NOSSO OBJETIVO</h1>
+                <h1 className="title">{title}</h1>
             </div>
+
+            <div id="divisor"></div>
         
-            <div id="boxPrincipal">                
-                <Informations icon={GrowIcon} 
-                            titulo={"Alavancar a carreira dos pequenos empreendedores"}
-                            texto={"Oferecemos uma plataforma que ajuda os pequenos e microempreendedores a aumentar sua visibilidade no mercado, alcançando mais clientes e impulsionando suas vendas."}
-                ></Informations>
-                <Informations icon={StoreIcon} 
-                            titulo={"Promover o comércio local"}
-                            texto={"Facilitando a conexão entre os empreendedores locais e os consumidores que desejam produtos e serviços de qualidade a preços acessíveis, incentivando o apoio ao comércio local e fortalecendo a economia da comunidade."}
-                ></Informations>
-                <Informations icon={PuzzleIcon} 
-                            titulo={"Fomentar parcerias e colaborações"}
-                            texto={"Estabelecendo parcerias estratégicas, como a colaboração com o Sebrae para capacitar novos empreendedores, a fim de oferecer suporte adicional e recursos educacionais para o crescimento e desenvolvimento dos negócios."}
-                ></Informations>
-            </div>
+            
         </section>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import Header from './components/header/header.jsx'
 import BarraLinks from './components/barraLinks/BarraLinks.jsx'
 import Footer from './components/footer/footer.jsx'
 import BoxInfo from './components/boxInfo/boxInfo.jsx'
+import GrowIcon from './assets/growIcon.svg';
+import StoreIcon from './assets/storeIcon.svg';
+import PuzzleIcon from './assets/puzzleIcon.svg'; 
+import Informations from './components/informations/informations.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,7 +19,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <br></br>
     </div>
     <Carrossel></Carrossel>
-    <BoxInfo></BoxInfo>
+    <BoxInfo title={"NOSSO OBJETIVO"}></BoxInfo>
+    <div>
+      <div id="boxInformations">                
+                <Informations icon={GrowIcon} 
+                            titulo={"Alavancar a carreira dos pequenos empreendedores"}
+                            texto={"Oferecemos uma plataforma que ajuda os pequenos e microempreendedores a aumentar sua visibilidade no mercado, alcançando mais clientes e impulsionando suas vendas."}
+                ></Informations>
+                <Informations icon={StoreIcon} 
+                            titulo={"Promover o comércio local"}
+                            texto={"Facilitando a conexão entre os empreendedores locais e os consumidores que desejam produtos e serviços de qualidade a preços acessíveis, incentivando o apoio ao comércio local e fortalecendo a economia da comunidade."}
+                ></Informations>
+                <Informations icon={PuzzleIcon} 
+                            titulo={"Fomentar parcerias e colaborações"}
+                            texto={"Estabelecendo parcerias estratégicas, como a colaboração com o Sebrae para capacitar novos empreendedores, a fim de oferecer suporte adicional e recursos educacionais para o crescimento e desenvolvimento dos negócios."}
+                ></Informations>
+            </div>
+    </div>
+
     <Footer></Footer>
   </React.StrictMode>,
 )
