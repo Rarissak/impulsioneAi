742fb0f2522d4f31bb78c29dee53189621332e84
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-14 23:39:22 -0300

    edt feat: Ajustes no componente Destaques

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Header from './components/header/header.jsx'
 import BarraLinks from './components/barraLinks/BarraLinks.jsx'
 import Footer from './components/footer/footer.jsx'
 import BoxInfo from './components/boxInfo/boxInfo.jsx'
+import Featured from './components/featured/featured.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +15,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div>
       <br></br>
     </div>
+    <Featured title={"NOSSOS DESTAQUES"}></Featured>
+        {/* <Featured titleBox={"NOSSOS DETAQUES"}
+              name1={Teste}
+              nincho1={teste}
+              name2={Teste}
+              nincho2={teste}
+              name3={Teste}
+              nincho3={teste}
+              name4={Teste}
+              nincho4={teste}
+              name5={Teste}
+              nincho5={teste}></Featured> */}
     <Carrossel></Carrossel>
     <BoxInfo></BoxInfo>
     <Footer></Footer>
