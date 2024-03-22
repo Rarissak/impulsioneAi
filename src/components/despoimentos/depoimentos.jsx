2d0ce7d2f4d9895217f52b1754a240a96580d7c5
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-21 23:44:31 -0300

    edit feat: Ajuste no compomente depoimentos

--- src/components/despoimentos/depoimentos.jsx
+++ src/components/despoimentos/depoimentos.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import './depoimentos.css';
+import StarIcon from '../../assets/starIcon.svg'
 
 function Depoimentos({imagemSrc, nome , nicho , review}) {
     return (
         <>
-            <div className='caixa'>
+            {/* <div className='caixa'>
                 <div className='caixa_top'>
                     <div className='caixa_top-img'>
                         <img src={imagemSrc} alt="Imagem" />
                     </div>
                     <div className='caixa_top-info'>
-                        <p>{nome}</p>
+                        <h3>{nome}</h3>
                         <p>{nicho}</p>
                     </div>
                 </div>
                 <div className='caixa_bottom'>
                     {review}
+                </div>
+                <div id='estrelas'>
+
+                </div>
+            </div> */}
+
+
+            <div className='caixa'>
+                <div className='caixa_top'>
+                    {/* Não precisa de uma div só pra imagem */}
+                    <img src={imagemSrc} alt="Imagem" />
+                    <div className='caixa_top-info'>
+                        {/* É bom diferenciar e dizer que um é um h3 e o outro um p */}
+                        <h3>{nome}</h3>
+                        <p>{nicho}</p>
+                    </div>
+                </div>
+                {/* Sugestão: aqui poderia ser só uma tag <p></p> */}
+                <p className='caixa_bottom'>
+                    {review}
+                </p>
+                <div id='estrelas'>
+                    <img src={StarIcon}/>
+                    <img src={StarIcon}/>
+                    <img src={StarIcon}/>
+                    <img src={StarIcon}/>
+                    <img src={StarIcon}/>
                 </div>
             </div>
         </>
