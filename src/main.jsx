534fa8d6f96dc3b0142e7a6a04d65fb28a518c04
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-03-25 19:44:58 -0300

    edit feat: ajustes no router

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, RouterProvider, Route, Routes } from 'react-router-dom';
+
+
 import './index.css'
 
 import AreaParceiro from './pages/UsuarioComum/AreaParceiro/index.jsx';
 import Vitrine from './pages/UsuarioComum/Vitrine/index.jsx';
 import Home from './pages/UsuarioComum/Home/index.jsx';
-import Login from './components/login/login.jsx';
-
-// Configurando o router
-import {
-  createBrowserRouter, RouterProvider, Route, Routes
-} from 'react-router-dom'
+import Login from './pages/UsuarioComum/login/login.jsx';
 
 
-<Router>
-  <Routes>
-    <Route path='/vitrine' Component={Vitrine} />
-    <Route path='/areaParceiro' Component={AreaParceiro} />
-  </Routes>
-</Router>
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/vitrine' Component={Vitrine} />
+        <Route path='/areaParceiro' Component={AreaParceiro} />
+        <Route path='/' Component={Home} />
+        <Route path='/login' Component={Login} />
+      </Routes>
+    </Router>
+  );
+}
+// export default App
+
+
 // Aqui a gente coloca todas as pages que vão ter
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home></Home>
-  },
-  {
-    path: "areaParceiro",
-    element: <AreaParceiro></AreaParceiro>,
-  },
-  {
-    path: "vitrine",
-    elements: <Vitrine></Vitrine>,
-  },
-  {
-    path: "login",
-    elements: <Login></Login>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home></Home>
+//   },
+//   {
+//     path: "areaParceiro",
+//     element: <AreaParceiro></AreaParceiro>,
+//   },
+//   {
+//     path: "vitrine",
+//     elements: <Vitrine></Vitrine>,
+//   },
+//   {
+//     path: "login",
+//     elements: <Login></Login>
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App></App>
+    {/* <RouterProvider router={router}></RouterProvider> */}
 
     {/* <AreaParceiro></AreaParceiro> */}
 
