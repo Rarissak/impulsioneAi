0113dba246933ffdd914c9adc29137dfbf0c17bb
Jhonnatas Mendonça <jhonnatas.m2000@gmail.com>
2024-03-25 23:53:20 -0300

    Edit feat: envio de e-mail automático assim que o cadastro é realizado.

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -43,8 +43,14 @@ function Form()
         try {
             const resposta = await axios.post('http://localhost:8080/empreendedores', dados);
             console.log(resposta.data);
-            
-           handleSubmitEndereco();
+
+            localStorage.setItem('cpf', dados.cpf);
+            localStorage.setItem('email', dados.email);
+
+            handleSubmitEndereco();
+            handleSubmitEmail();
+           
+        
            alert("Cadastro realizado com sucesso!");
            window.location.href='./login';
         } catch (erro) {
@@ -85,9 +91,29 @@ function Form()
     };
 
 
+    const [dadosEmail, setDadosEmail] = useState({
+        ownerRef: "Suporte",
+        emailFrom: "impulsioneai@gmail.com",
+        emailTo: localStorage.getItem('email'),
+        subject: "Bem-vindo (a) ao ImpulsioneAI",
+        text: "Bem-vindo(a) ao ImpulsioneAi! Estamos muito felizes em tê-lo(a) conosco! A nossa plataforma foi criada para lhe ajudar na divulgação do seu trabalho. Qualquer dúvida é só entrar em contato!"
+    });
 
+    const handleSubmitEmail = async () => {
+        event.preventDefault();
 
+        try {
+            const resposta = await axios.post('http://localhost:8080/email', dadosEmail);
+            console.log(resposta.data);
+            
+        } catch (erro) {
+            console.error('Ocorreu um erro ao enviar o e-mail:', erro);
+        }
+    };
 
+    const handleChangeEmail = (event) => {
+        setDadosEmail({ ...dadosEmail, [event.target.name]: event.target.value });
+    };
 
 
     return(
